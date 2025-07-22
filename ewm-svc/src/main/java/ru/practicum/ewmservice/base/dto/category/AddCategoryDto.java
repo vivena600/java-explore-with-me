6a6dec93df05f4247416b69174e2f20b95e1c195
Vivi <golovinad84@gmail.com>
2025-07-22 11:29:52 +0300

    fix category validation

--- ewm-svc/src/main/java/ru/practicum/ewmservice/base/dto/category/AddCategoryDto.java
+++ ewm-svc/src/main/java/ru/practicum/ewmservice/base/dto/category/AddCategoryDto.java
@@ -1,10 +1,14 @@
 package ru.practicum.ewmservice.base.dto.category;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 public class AddCategoryDto {
+    @NotBlank
+    @Length(max = 50, min = 1)
     private String name;
 }
